93d2a6517d81e885aedd07cf40ce997c5a142dfa
RahulPhpDev <44345589+RahulPhpDev@users.noreply.github.com>
2020-03-26 19:33:30 +0530

    Use of Scope with Eloquent

--- app/Article.php
+++ app/Article.php
@@ -3,8 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Builder;
+use App\Scope\ArticleGlobalScope;
 class Article extends Model
 {
-    // Master
+    protected static function boot() {
+    	parent::boot();
+    	static::addGlobalScope(new ArticleGlobalScope());
+    	// static::addGlobalScope('user_filer', function (Builder $builder) {
+    	// 	$builder->where('user_id', 1)->whereYear('created_at', 2020);
+    	// });
+    }
+
+
+
+    public function scopeCurrentYearArticle($query, $year = 2020) {
+    	// dd(date('Y'));
+    	return $query->whereUserId(1)->whereYear('created_at', $year);
+    }
+
+
+
 }
